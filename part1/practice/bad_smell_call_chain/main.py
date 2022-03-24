50648e20a57458a47f73e63564db5ad5f3bfe80e
Marina <petrukmn@yandex.ru>
2022-03-24 14:19:45 +0300

    lesson 22

--- part1/practice/bad_smell_call_chain/main.py
+++ part1/practice/bad_smell_call_chain/main.py
@@ -1,45 +1,59 @@
 # Измените класс Person так, чтобы его методы 
 # оперировали внутренним состоянием, 
 # а не использовали цепочку вызовов объектов
-
-class Room:
-    def get_name(self):
-        return 42
-
-
-class Street:
-    def get_room(self) -> Room:
-        return Room()
-
-
 class City:
-    def get_street(self) -> Street:
-        return Street()
+    def __init__(self, name: str, population: int):
+        self._name = name
+        self._population = population
 
-    def population(self):
-        return 100500
+    def get_name(self):
+        return self._name
 
-
-class Country:
-    def get_city(self) -> City:
-        return City()
-
-
-class Planet:
-    def get_contry(self) -> Country:
-        return Country()
+    def get_population(self):
+        return self._population
 
 
 class Person:
-    def __init__(self):
-        self.planet = Planet()
+    def __init__(self, room_number: int, street: str, country: str, planet: str):
+        self._room_number = room_number
+        self._street = street
+        self._city = city.get_name()
+        self._city_population = city.get_population()
+        self._country = country
+        self._planet = planet
 
-    def get_person_room(self):
-        return self.planet.get_contry().get_city().get_street().get_room().get_name()
+    def get_person_planet(self):
+        return self._planet
+
+    def get_person_country(self):
+        return self._country
+
+    def get_person_city(self):
+        return self._city
 
     def get_city_population(self):
-        return self.planet.get_contry().get_city().population()
+        return self._city_population
+
+    def get_person_street(self):
+        return self._street
+
+    def get_person_room(self):
+        return self._room_number
+
+    def person_data(self):
+        return {"planet": self.get_person_planet(),
+                "country": self.get_person_country(),
+                "city": self.get_person_city(),
+                "city population": self.get_city_population(),
+                "street": self.get_person_street(),
+                "room": self.get_person_room()
+                }
 
 
 # TODO после выполнения задания попробуйте
 # сделать экземпляр класса person и вызвать новые методы.
+
+
+city = City("Moscow", 10_000_000)
+person = Person(221, "Lenina", "Russia", "Earth")
+print(person.person_data())
